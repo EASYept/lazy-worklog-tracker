c52d02d3186ce4df9e69cdd77dc64c0b4018672f
EASYept <bybl1k@yandex.ru>
2025-11-03 20:22:08 +0400

    Update main.py

--- src/lazy_worklog_tracker/main.py
+++ src/lazy_worklog_tracker/main.py
@@ -1,11 +1,10 @@
 from __future__ import annotations
 
 
-from lazy_worklog_tracker.app import WorklogTracker
 from lazy_worklog_tracker.config import Container
 
 
 if __name__ == "__main__":
     conteiner = Container()
-    app = WorklogTracker(conteiner.repo())
+    app = conteiner.app()
     app.run()
